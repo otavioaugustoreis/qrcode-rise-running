050c348aeb45f84a1e475556757dd7358548acdd
Otavbio Augusto Reis Almeida <oaugusto265@gmail.com>
2025-11-13 13:45:59 -0300

    feat: Adicionando banco

---
+++ RiseRunning-ScannerCode/Model/Repository/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RiseRunning_ScannerCode.Model.Entity;
+
+namespace RiseRunning_ScannerCode.Model.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<RunnerEntity> TB_RUNNERS { get; set; }
+    }
+}
--- RiseRunning-ScannerCode/Program.cs
+++ RiseRunning-ScannerCode/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using RiseRunning_ScannerCode.Model.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlite("Data Source=runners.db"));
 builder.Services.AddSwaggerGen();
 builder.Services.AddMemoryCache();
 
